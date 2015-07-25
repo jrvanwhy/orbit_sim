e6a795868db19214aeafd5a540ffa8fa23fcb5a5
Johnathan Van Why <jrvanwhy@gmail.com>
2015-07-24 22:03:28 -0700

    Simulates an orbit now. Around 40 million timesteps per second with a basic symplectic Euler integrator.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
-extern crate nalgebra;
-use nalgebra::Vec3;
+extern crate nalgebra as na;
+use na::Vec3;
 
-fn calc_accel(pos: Vec3<f64>, vel: Vec3<f64>) -> Vec3<f64> {
-	-pos
+const EARTH_RADIUS: f64 = 6_371_000.;
+const EARTH_GACCEL: f64 = 9.81;
+const GRAV_K: f64 = -EARTH_GACCEL * EARTH_RADIUS * EARTH_RADIUS;
+
+fn calc_accel(pos: &Vec3<f64>, _vel: &Vec3<f64>) -> Vec3<f64> {
+	let r_sqr = na::dot(pos, pos);
+	*pos * (GRAV_K / (r_sqr*r_sqr*r_sqr).sqrt())
+}
+
+fn ode_step(pos: &mut Vec3<f64>, vel: &mut Vec3<f64>) {
+	const DT: f64 = 0.001;
+	
+	*vel = *vel + calc_accel(&pos, &vel) * DT;
+	*pos = *pos + *vel * DT;
 }
 
 fn main() {
+	let mut pos = Vec3::new(EARTH_RADIUS + 300_000f64, 0., 0.);
+	let mut vel = Vec3::new(0., (-pos.x * calc_accel(&pos, &Vec3::new(0f64, 0., 0.)).x).sqrt(), 0.);
+
+	while pos.y >= 0. {
+		ode_step(&mut pos, &mut vel);
+	}
+
+	while pos.y < 0. {
+		ode_step(&mut pos, &mut vel);
+	}
+
+	println!("Position: ({}, {}, {}).", pos.x, pos.y, pos.z);
 }
